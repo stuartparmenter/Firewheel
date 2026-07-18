e08be0a2bc0d56e9c3372721f18dc62ddeb8d715
Stuart Parmenter <pavlov@pavlov.net>
2026-07-17 22:29:06 -0700

    glam 33

--- crates/firewheel-core/src/diff/leaf.rs
+++ crates/firewheel-core/src/diff/leaf.rs
@@ -160,6 +160,11 @@ primitive_diff!(glam_32::Vec2, Vector2D);
 #[cfg(feature = "glam-32")]
 primitive_diff!(glam_32::Vec3, Vector3D);
 
+#[cfg(feature = "glam-33")]
+primitive_diff!(glam_33::Vec2, Vector2D);
+#[cfg(feature = "glam-33")]
+primitive_diff!(glam_33::Vec3, Vector3D);
+
 impl<A: ?Sized + Send + Sync + 'static> Diff for ArcGc<A> {
     fn diff<E: EventQueue>(&self, baseline: &Self, path: PathBuilder, event_queue: &mut E) {
         if !ArcGc::ptr_eq(self, baseline) {
@@ -503,3 +508,8 @@ trivial_notify!(glam_31::Vec3);
 trivial_notify!(glam_32::Vec2);
 #[cfg(feature = "glam-32")]
 trivial_notify!(glam_32::Vec3);
+
+#[cfg(feature = "glam-33")]
+trivial_notify!(glam_33::Vec2);
+#[cfg(feature = "glam-33")]
+trivial_notify!(glam_33::Vec3);
